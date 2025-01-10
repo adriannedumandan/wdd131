fc1b085b9d4200d732499001d1f2060c6e2217d8
adriannedumandan <adriannedumandan25@gmail.com@byui.edu>
2025-01-10 14:53:40 +0800

    font text colors

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,6 +1,6 @@
 
 const currentYear = new Date().getFullYear();
-document.getElementById('currentyear').textContent = `© ${currentYear} Your Name`;
+document.getElementById('currentyear').textContent = ` ${currentYear} 🏝️ Adrianne Dumandan 🏝️ Philippines`;
 
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
