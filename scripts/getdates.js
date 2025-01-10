c6adaafa7bb5f137b0f205728f944eebf21308b6
adriannedumandan <adriannedumandan25@gmail.com@byui.edu>
2025-01-10 12:50:12 +0800

    js changes

--- scripts/getdates.js
+++ scripts/getdates.js
@@ -1,11 +1,6 @@
-// Get the current year
-const currentYear = new Date().getFullYear();
 
-// Output the current year in the footer's first paragraph
+const currentYear = new Date().getFullYear();
 document.getElementById('copyright').textContent = `© ${currentYear} Your Name`;
 
-// Get the last modified date of the document
 const lastModified = document.lastModified;
-
-// Output the last modified date in the footer's second paragraph
 document.getElementById('last-modified').textContent = `Last modified: ${lastModified}`;
