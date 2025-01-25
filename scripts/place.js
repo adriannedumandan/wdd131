06bfa8060fa858bd6a6b64181ce521e23c155b4c
adriannedumandan <adriannedumandan25@gmail.com@byui.edu>
2025-01-26 07:56:32 +0800

    changes added in css

--- scripts/place.js
+++ scripts/place.js
@@ -5,3 +5,19 @@ document.getElementById('currentyear').textContent = ` ${currentYear} 🏝️ Ad
 const lastModified = document.lastModified;
 document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
 
+
+function calculateWindChill(temperature, windSpeed) {
+    return (13.12 + 0.6215 * temperature - 11.37 * Math.pow(windSpeed, 0.16) + 0.3965 * temperature * Math.pow(windSpeed, 0.16)).toFixed(1);
+}
+
+
+const temperature = 25; 
+const windSpeed = 3; 
+
+
+if (temperature <= 10 && windSpeed > 4.8) {
+    const windChill = calculateWindChill(temperature, windSpeed);
+    document.querySelector('.weather .value:last-child').textContent = `${windChill} °C`;
+} else {
+    document.querySelector('.weather .value:last-child').textContent = 'N/A';
+}
