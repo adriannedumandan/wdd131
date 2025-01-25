51b12b47cdb6d078334d583f9d9d4c83c0d57d6d
adriannedumandan <adriannedumandan25@gmail.com@byui.edu>
2025-01-26 07:51:20 +0800

    place html and css added

---
+++ scripts/place.js
@@ -0,0 +1,7 @@
+
+const currentYear = new Date().getFullYear();
+document.getElementById('currentyear').textContent = ` ${currentYear} 🏝️ Adrianne Dumandan 🏝️ Philippines`;
+
+const lastModified = document.lastModified;
+document.getElementById('lastModified').textContent = `Last modified: ${lastModified}`;
+
